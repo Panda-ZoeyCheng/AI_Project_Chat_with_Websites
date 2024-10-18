1b46d23e8acb2c0006ed326e5e0c38bf071d8036
zoeycheng <zoeycheng98@gmail.com>
2024-10-18 21:46:48 +1000

    Debug

--- src/app.py
+++ src/app.py
@@ -13,13 +13,6 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(page_title="PlotBot", page_icon=":robot_face:")
 st.markdown("<h1 style='text-align: center;'>PlotBot</h1>", unsafe_allow_html=True)
 
-upload_file = st.sidebar.file_uploader("Upload CSV file", type="csv")
-if upload_file is not None:
-    df = pd.read_csv(upload_file)
-    schema = df.dtypes.to_string()
-    query = None
-
-
 if "past" not in st.session_state:
     st.session_state["past"] = []
 if "generated" not in st.session_state:
@@ -32,10 +25,19 @@ if "messages" not in st.session_state:
     ]
 
 st.sidebar.title("Sidebar")
+
+upload_file = st.sidebar.file_uploader("Upload CSV file", type="csv")
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    schema = df.dtypes.to_string()
+    query = None
+
 counter_placeholder = st.sidebar.empty()
 clear_button = st.sidebar.button("Clear conversation", key="clear")
 
+
 model_name = 'gpt-3.5-turbo'
+
 
 if clear_button:
     st.session_state["generated"] = []
@@ -75,7 +77,7 @@ def create_plot_from_response(response_text):
     #     st.error(f"Error executing generated code: {e}")
 
 def generate_response(query):
-    st.session_state["messages"].append({"role": "user", "content": query})
+    # st.session_state["messages"].append({"role": "user", "content": query})
     prompt = f""" You are a matplotlib and seaborn expert.
     You answer questions related to data
     Your also write code for creating visualization charts based on input query.
@@ -130,41 +132,66 @@ def generate_response(query):
         # image_path = os.path.join(os.getcwd(), "src", "plots", "plot.png")
         # st.write(f"Image path: {image_path}")
 
-        full_response = "Chart is shown below"
+        return "Chart is shown below"
 
         # if os.path.exists(image_path):
         #     st.session_state["graph"].append(Image.open(image_path))
         #     full_response = "Chart is shown below"
     else:
-        st.session_state["graph"].append(None)
+        # st.session_state["graph"].append(None)
+        return  full_response
     
+    # st.session_state["messages"].append({"role": "assistant", "content": full_response})
+
+# response_container = st.container()
+# container = st.container()
+
+def display_messages():
+    for message in st.session_state["messages"]:
+        with st.chat_message(message["role"]):
+            if message["role"] == "graph":
+                st.image(message["content"], width=700)
+            else:
+                st.markdown(message["content"])
+
+input = st.chat_message("Please describe what graph you would like to show?")
+
+if input:
+    with st.chat_message("user"):
+        st.markdown(input)
+    st.session_state.message.append({"role": "user", "content": input})
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = generate_response(input)
+        message_placeholder.markdown(full_response)
+
     st.session_state["messages"].append({"role": "assistant", "content": full_response})
 
-response_container = st.container()
-container = st.container()
+# with container:
+#     with st.form(key="my_form", clear_on_submit=True):
+#         user_input = st.text_area("You:", key="input", height=70)
+#         submit_button = st.form_submit_button(label="send")
 
-with container:
-    with st.form(key="my_form", clear_on_submit=True):
-        user_input = st.text_area("You:", key="input", height=70)
-        submit_button = st.form_submit_button(label="send")
-
-    if submit_button and user_input:
-        output = generate_response(user_input)
-        st.session_state["past"].append(user_input)
-        st.session_state["generated"].append(output)
+#     if submit_button and user_input:
+#         output = generate_response(user_input)
+#         st.session_state["past"].append(user_input)
+#         st.session_state["generated"].append(output)
     
 
-if st.session_state["generated"]:
-    with response_container:
-        for i in range(len(st.session_state["generated"])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="identicon")
-            message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
+# if st.session_state["generated"]:
+#     with response_container:
+#         for i in range(len(st.session_state["generated"])):
+#             message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="identicon")
+#             message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
 
-            try:
-                # st.image(st.session_state["graph"][i], width=10)
-                if st.session_state["graph"][i]:
-                    st.image(st.session_state["graph"][i], width=700)
-                else:
-                    st.warning("No image to display.")
-            except Exception as e:
-                st.error(f"Error displaying image: {e}")
+#             try:
+#                 # st.image(st.session_state["graph"][i], width=10)
+#                 if st.session_state["graph"][i]:
+#                     st.image(st.session_state["graph"][i], width=700)
+#                 else:
+#                     st.warning("No image to display.")
+#             except Exception as e:
+#                 st.error(f"Error displaying image: {e}")
+
+display_messages()
